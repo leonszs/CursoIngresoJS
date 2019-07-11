4ab0b3ee2588ae999428e1374bf1b904552ac48b
leonszs <47385872+leonszs@users.noreply.github.com>
2019-07-11 12:28:10 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,8 +1,27 @@
 function mostrar()
-{
+{   
+
+	var numero;
+	var suma = 0;
+	var promedio;
 
 	var contador=0;
 	var acumulador=0;
+
+
+
+	while (contador<5){
+		numero = parseInt(prompt ("ingrese un numero"));
+		suma +=numero;
+
+         
+
+
+
+		contador++;
+	}
+
+	promedio = suma/5;
 
 
 
@@ -10,3 +29,8 @@ document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
 
 }//FIN DE LA FUNCIÓN
+
+// calcular el promedio va fuera de bucle
+
+
+//el contador se usa dentro del bucle.
